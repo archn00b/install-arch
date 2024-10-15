ca4a7216394479e75f8be1509d22984a064f279f
archn00b <archn00b2024@gmail.com>
2024-10-15 16:40:48 +0000

    added to the script

--- formatdisk.sh
+++ formatdisk.sh
@@ -30,3 +30,27 @@ mkfs.fat -F 32 "${Arch_Disk}1"
 mkswap "${Arch_Disk}2"
 mkfs.ext4 "${Arch_Disk}3"
 
+# MOUNTING THE FILE SYSTEM
+mount "${Arch_Disk}3" /mnt
+mount "${Arch_Disk}1" /mnt/boot
+swapon "${Arch_Disk}2"
+
+# INSTAL ESSENTIAL PACKAGES
+pacstrap -K /mnt base linux linux-firmaware
+
+# CONFIGURE THE SYSTEM
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# CHANGE ROOT INTO THE NEW SYSTEM
+arch-chroot /mnt
+/bin/ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime
+hwclock --systohc
+/bin/sed -i '178s/.//' /etc/locale.gen
+locale-gen
+/bin/echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+/bin/echo "arch" >> /etc/hostname
+/bin/echo "127.0.0.1 localhost" >> /etc/hosts
+/bin/echo "::1       localhost" >> /etc/hosts
+/bin/echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+/bin/echo root:123 | chpasswd
+
