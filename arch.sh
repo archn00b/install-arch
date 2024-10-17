756849b853f0ac9f3b2a555f63a3a059d1180dd3
archn00b <archn00b2024@gmail.com>
2024-10-17 00:29:07 +0000

    changed the script

--- arch.sh
+++ arch.sh
@@ -18,3 +18,21 @@ pacstrap -K /mnt base linux linux-firmware
 
 # CONFIGURE THE SYSTEM
 genfstab -U /mnt >> /mnt/etc/fstab
+
+# Mount Kernel Virtual File Systems
+mount -t proc proc $Arch/proc
+mount -t sysfs sysfs $Arch/sys
+mount -t devtmpfs devtmpfs $Arch/dev
+mount -t tmpfs tmpfs $Arch/dev/shm
+mount -t devpts devpts $Arch/dev/pts
+
+# Copy /etc/hosts
+/bin/cp -f /etc/hosts $Arch/etc/
+
+# Copy /etc/resolv.conf 
+/bin/cp -f /etc/resolv.conf $Arch/etc/resolv.conf
+
+# Link /etc/mtab
+chroot $Arch rm /etc/mtab 2> /dev/null 
+chroot $Arch ln -s /proc/mounts /etc/mtab
+echo "Inside of chroot"
