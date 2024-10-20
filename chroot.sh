6ae50956a96f77cf30c996c719505c809518dbce
archn00b <archn00b2024@gmail.com>
2024-10-20 06:44:32 +0000

    changes a bug

--- chroot.sh
+++ chroot.sh
@@ -2,21 +2,21 @@
 
 
 # shellcheck disable=SC2154
-arch-chroot /mnt /bin/bash <<EOF
+arch-chroot /mnt /root /bin/bash <<EOF
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 sleep1
 hwclock --systohc
 sleep 1
+sleep 1
 sed -i 's/^#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
 sleep 1
 locale-gen
-sleep 2
+sleep 1
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
-passwd
 echo root:123 | chpasswd
 sleep 5
 
@@ -27,12 +27,10 @@ systemctl enable NetworkManager
 systemctl enable sddm
 sleep 5
 
-useradd -m -G wheel archnoob -s bash archnoob 
-passwd archnoob
+useradd -m archnoob
 echo archnoob:123 | chpasswd
 sleep 5
 
-sudo touch /etc/sudoers.d/archnoob
 echo "archnoob ALL=(ALL) ALL" >> /etc/sudoers.d/archnoob
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
