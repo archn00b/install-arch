323e5f90824cef08e98f0e620b61ec00ecc019c9
archn00b <archn00b2024@gmail.com>
2024-10-20 05:42:15 +0000

    changed code

--- chroot.sh
+++ chroot.sh
@@ -2,8 +2,8 @@
 
 
 # shellcheck disable=SC2154
-arch-chroot "$Arch_Root" /bin/bash <<EOF
-ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime
+arch-chroot /mnt /bin/bash <<EOF
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 hwclock --systohc
 sed -i 's/^#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
 locale-gen
@@ -14,13 +14,10 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 echo root:123 | chpasswd
 
-pacman -S grub efibootmgr networkmanager network-manager-applet  
-
-
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB  
-grub-mkconfig -o /boot/grub/grub.cfg
+pacman -S grub efibootmgr networkmanager network-manager-applet xorg xfce4 xfce4-goodies sddm firefox alacritty nvidia nvidia-utils nvidia-settings 
 
 systemctl enable NetworkManager
+systemctl enable sddm
 
 
 useradd -m archnoob
@@ -28,6 +25,8 @@ echo archnoob:123 | chpasswd
 
 echo "archnoob ALL=(ALL) ALL" >> /etc/sudoers.d/archnoob
 
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB  
+grub-mkconfig -o /boot/grub/grub.cfg
+
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
-exit
 EOF
