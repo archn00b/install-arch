8acb894768f5fa735a83ecbd35289b6c98991664
archn00b <archn00b2024@gmail.com>
2026-09-15 22:36:25 -0400

    changed the script chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -1,43 +1,149 @@
 #!/usr/bin/env bash
 
+# ==========================================
+# ARCH LINUX CHROOT CONFIGURATION
+# ==========================================
 
-# shellcheck disable=SC2154
-arch-chroot /mnt /root /bin/bash <<EOF
+arch-chroot /mnt /bin/bash <<EOF
+
+# ------------------------------------------
+# Timezone
+# ------------------------------------------
+
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
-sleep1
-hwclock --systohc
-sleep 1
-sleep 1
-sed -i 's/^#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
-sleep 1
-locale-gen
-sleep 1
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "arch" >> /etc/hostname
-echo "127.0.0.1 localhost" >> /etc/hosts
-echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
-echo root:123 | chpasswd
-sleep 5
 
-pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet xorg xfce4 xfce4-goodies sddm firefox
-sleep 5
+sleep 1
+
+hwclock --systohc
+
+sleep 1
+
+
+# ------------------------------------------
+# Locale
+# ------------------------------------------
+
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+
+sleep 1
+
+locale-gen
+
+sleep 1
+
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+sleep 1
+
+
+# ------------------------------------------
+# Hostname
+# ------------------------------------------
+
+echo "arch" > /etc/hostname
+
+sleep 1
+
+
+# ------------------------------------------
+# Hosts
+# ------------------------------------------
+
+cat > /etc/hosts <<HOSTS
+127.0.0.1   localhost
+::1         localhost
+127.0.1.1   arch.localdomain arch
+HOSTS
+
+sleep 1
+
+
+# ------------------------------------------
+# Root Password
+# ------------------------------------------
+
+echo "root:123" | chpasswd
+
+sleep 2
+
+
+# ------------------------------------------
+# Install Packages
+# ------------------------------------------
+
+pacman -S --noconfirm \
+    grub \
+    efibootmgr \
+    networkmanager \
+    network-manager-applet \
+
+sleep 3
+
+
+# ------------------------------------------
+# Enable Services
+# ------------------------------------------
 
 systemctl enable NetworkManager
-systemctl enable sddm
-sleep 5
+
+sleep 1
+
+# ------------------------------------------
+# Create User
+# ------------------------------------------
 
 useradd -m archnoob
-echo archnoob:123 | chpasswd
-sleep 5
 
-echo "archnoob ALL=(ALL) ALL" >> /etc/sudoers.d/archnoob
+sleep 1
 
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-sleep 5
+echo "archnoob:123" | chpasswd
+
+sleep 1
+
+
+# ------------------------------------------
+# Give User Sudo Access
+# ------------------------------------------
+
+echo "archnoob ALL=(ALL) ALL" > /etc/sudoers.d/archnoob
+
+chmod 440 /etc/sudoers.d/archnoob
+
+sleep 1
+
+
+# ------------------------------------------
+# Install GRUB
+# ------------------------------------------
+
+grub-install \
+    --target=x86_64-efi \
+    --efi-directory=/boot \
+    --bootloader-id=GRUB
+
+sleep 3
+
+
+# ------------------------------------------
+# Generate GRUB Configuration
+# ------------------------------------------
+
 grub-mkconfig -o /boot/grub/grub.cfg
-sleep 5
 
-printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
-sleep 5
+sleep 3
+
+
+# ------------------------------------------
+# Finished
+# ------------------------------------------
+
+printf '\\e[1;32m\\n==========================================\\n'
+printf '        Arch Linux Setup Complete!\\n'
+printf '==========================================\\n'
+printf '\\n'
+printf 'Type: exit\\n'
+printf 'Then: umount -R /mnt\\n'
+printf 'Then: reboot\\n'
+printf '\\n\\e[0m'
+
 EOF
